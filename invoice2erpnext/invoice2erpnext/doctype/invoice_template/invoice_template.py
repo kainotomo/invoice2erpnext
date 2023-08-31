cefaa3ee7ea08587b570a6d9a19f84dfca6c4468
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-08-31 05:47:54 +0000

    added invoice template doctype

---
+++ invoice2erpnext/invoice2erpnext/doctype/invoice_template/invoice_template.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, KAINOTOMO PH LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class InvoiceTemplate(Document):
+	pass
