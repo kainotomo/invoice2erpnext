b80b26d2e3841e14ecc04e3c22bc78f81241f64c
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-09-02 04:15:22 +0000

    succesfully saved purchase invoice

--- invoice2erpnext/invoice2erpnext/doctype/invoice_file/invoice_file.js
+++ invoice2erpnext/invoice2erpnext/doctype/invoice_file/invoice_file.js
@@ -2,7 +2,36 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Invoice File', {
+	setup: function(frm) {
+		frm.custom_make_buttons = {
+			'Sales Invoice': 'Sales Invoice'
+		}
+	},
 	refresh: function(frm) {
-		frm.set_df_property("html_render", "options", frm.doc.html_table);
-	}
+		if (frm.doc.html_table) {
+			frm.set_df_property("html_render", "options", frm.doc.html_table);
+			frm.add_custom_button(__('Purchase Invoice'), function () {
+				frappe.confirm('Are you sure you want to proceed?', function () {
+					frappe.call({
+						method: "invoice2erpnext.invoice2erpnext.doctype.invoice_file.invoice_file.make_purchase_invoice",
+						args: {
+							source_name: frm.doc.name
+						},
+						callback: function (response) {
+							if (response.message.errors) {
+								frappe.msgprint("Something went wrong.", 'Error');
+							} else {
+								//frappe.set_route(['List', "Purchase Invoice", "Purchase Invoice"], { 'status': 'Draft' })
+								frappe.msgprint("Succesfully created document.");
+							}
+						}
+					});
+				}, function () {
+					// action to perform if No is selected
+				});
+			}, __('Create'));
+		} else {
+			frm.set_df_property("html_render", "options", "<p>-</p>");
+		}
+	},	
 });
--- invoice2erpnext/invoice2erpnext/doctype/invoice_file/invoice_file.py
+++ invoice2erpnext/invoice2erpnext/doctype/invoice_file/invoice_file.py
@@ -9,6 +9,7 @@ import datetime
 from invoice2data import extract_data
 from invoice2data.extract.loader import read_templates
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class InvoiceFile(Document):
 
@@ -26,3 +27,58 @@ class InvoiceFile(Document):
 					'result': self.result,
 					'html_table': self.html_table
 				})
+		else:
+			frappe.db.set_value('Invoice File', self.name, {
+					'result': None,
+					'html_table': None
+				})
+			
+@frappe.whitelist()
+def make_purchase_invoice(source_name):
+	invoice_file_doc = frappe.get_doc("Invoice File", source_name)
+	result = json.loads(invoice_file_doc.result)
+
+	pi = frappe.new_doc("Purchase Invoice")
+	pi.supplier = result["issuer"]
+	pi.currency = result.get('currency', None)
+	pi.conversion_rate = result.get('conversion_rate', 1)
+	pi.is_return = result.get('is_return', None)
+	pi.return_against = result.get('return_against', None)
+	pi.is_subcontracted = result.get('is_subcontracted', 0)
+	pi.supplier_warehouse = result.get('supplier_warehouse', None)
+	pi.cost_center = result.get('cost_center', None)
+	
+	pi.posting_date = result.get('date', frappe.utils.today())
+	pi.due_date = result.get('date', None)
+	pi.bill_no = result.get('invoice_number', None)
+	pi.bill_date = result.get('bill_date', pi.posting_date)
+
+	pi.append(
+		"items",
+		{
+			"item_code": result['item_code'],
+			"warehouse": result.get('warehouse', None),
+			"qty": result.get('qty', 1),
+			"received_qty":result.get('received_qty', 0),
+			"rejected_qty": result.get('rejected_qty', 0),
+			"rate": result.get('amount', 0),
+			"price_list_rate": result.get('price_list_rate', 50),
+			"expense_account": result.get('expense_account', None),
+			"discount_account": result.get('discount_account', None),
+			"discount_amount": result.get('discount_amount', 0),
+			"conversion_factor": 1.0,
+			"serial_no": result.get('serial_no', None),
+			"stock_uom": result.get('stock_uom', None),
+			"cost_center": result.get('cost_center', None),
+			"project": result.get('project', None),
+			"rejected_warehouse": result.get('rejected_warehouse', None),
+			"rejected_serial_no": result.get('rejected_serial_no', None),
+			"asset_location": result.get('asset_location', None),
+			"allow_zero_valuation_rate": result.get('allow_zero_valuation_rate', 0),
+		},
+	)
+	
+	pi.save();
+	return
+
+
