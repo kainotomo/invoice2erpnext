ee233e3fa8aee32451cba8bc7511d5c7edf230c7
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-09-04 04:58:32 +0000

    abitlity to create purchase order

--- invoice2erpnext/invoice2erpnext/doctype/invoice_file/invoice_file.js
+++ invoice2erpnext/invoice2erpnext/doctype/invoice_file/invoice_file.js
@@ -10,12 +10,35 @@ frappe.ui.form.on('Invoice File', {
 	refresh: function(frm) {
 		if (frm.doc.html_table) {
 			frm.set_df_property("html_render", "options", frm.doc.html_table);
+
+			frm.add_custom_button(__('Purchase Order'), function () {
+				frappe.confirm('Are you sure you want to proceed?', function () {
+					frappe.call({
+						method: "invoice2erpnext.invoice2erpnext.doctype.invoice_file.invoice_file.make_purchase_invoice",
+						args: {
+							source_name: frm.doc.name,
+							doc_type: "Purchase Order"
+						},
+						callback: function (response) {
+							if (response.message.errors) {
+								frappe.msgprint("Something went wrong.", 'Error');
+							} else {
+								frappe.set_route("Form", "Purchase Order", response.message)
+							}
+						}
+					});
+				}, function () {
+					// action to perform if No is selected
+				});
+			}, __('Create'));
+
 			frm.add_custom_button(__('Purchase Invoice'), function () {
 				frappe.confirm('Are you sure you want to proceed?', function () {
 					frappe.call({
 						method: "invoice2erpnext.invoice2erpnext.doctype.invoice_file.invoice_file.make_purchase_invoice",
 						args: {
-							source_name: frm.doc.name
+							source_name: frm.doc.name,
+							doc_type: "Purchase Invoice"
 						},
 						callback: function (response) {
 							if (response.message.errors) {
