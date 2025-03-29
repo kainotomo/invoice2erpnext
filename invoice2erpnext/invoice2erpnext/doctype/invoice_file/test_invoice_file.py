5571e30f3016906771fc8e081e84f29e3062f4e5
Panayiotis Halouvas <phalouvas@gmail.com>
2025-03-29 04:39:41 +0000

    refactor: remove unused test files and related code for invoice templates and files

--- invoice2erpnext/invoice2erpnext/doctype/invoice_file/test_invoice_file.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, KAINOTOMO PH LTD and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestInvoiceFile(FrappeTestCase):
-	pass
