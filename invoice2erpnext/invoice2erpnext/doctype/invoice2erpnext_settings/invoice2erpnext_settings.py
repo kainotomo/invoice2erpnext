5cb0e81e191a3dfadc5bf7d7c27532f51c25ed75
Panayiotis Halouvas <phalouvas@gmail.com>
2025-03-29 04:37:58 +0000

    feat: add Invoice2Erpnext Settings DocType with API key and secret fields

---
+++ invoice2erpnext/invoice2erpnext/doctype/invoice2erpnext_settings/invoice2erpnext_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, KAINOTOMO PH LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Invoice2ErpnextSettings(Document):
+	pass
